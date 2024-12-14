5f4c3ea92458be409fdca761aebc121e85480850
balazzska <balazzska92@gmail.com>
2024-12-14 17:36:24 +0100

    ---

--- 12/12.py
+++ 12/12.py
@@ -1,9 +1,11 @@
 from pprint import pprint
 import numpy as np
+from tqdm import tqdm
 
 
 file = "example"
-# file = "input"
+file = "example2"
+file = "input"
 
 inputlines = open(f"12\\12-{file}.txt", "r").readlines()
 inputlines = [line.strip() for line in inputlines]
@@ -12,5 +14,49 @@ inputlines = [line.strip() for line in inputlines]
 chars = np.array([list(line) for line in inputlines])
 
 unique_chars = np.unique(chars)
+DIRECTIONS = [
+    np.array([0, -1]),
+    np.array([1, 0]),
+    np.array([0, 1]),
+    np.array([-1, 0]),
+]
 
 print(unique_chars)
+
+# create a dictionary with the unique characters as keys and the values as the coordinates of the character
+char_dict = {}
+for char in unique_chars:
+    char_dict[char] = [p for p in np.argwhere(chars == char)]
+
+
+regions = []
+for char in tqdm(char_dict.keys()):
+    while len(char_dict[char]) > 0:
+        print("new region")
+
+        region = []
+        region.append(char_dict[char].pop(0))
+        head = region.copy()
+
+        changed = True
+        while changed:
+            changed = False
+            for r in region:
+                # find if neighbors are in the same region
+                for dir in DIRECTIONS:
+                    neigh = r + dir
+                    if np.any(np.all(neigh == char_dict[char], axis=1)):
+                        region.append(neigh)
+                        head = [p for p in head if not np.array_equal(p, neigh)]
+
+                        head.append(neigh)
+
+                        char_dict[char] = [
+                            p for p in char_dict[char] if not np.array_equal(p, neigh)
+                        ]
+                        if(len(char_dict[char]) == 0):
+                            break
+                        changed = True
+        regions.append(region)
+
+print(len(regions))
---
+++ 14/14.py
@@ -0,0 +1,128 @@
+from pprint import pprint
+import re
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+
+file, MAP_SIZE = "example", [11, 7]
+file, MAP_SIZE = "input", [101, 103]
+
+inputlines = open(f"14\\14-{file}.txt", "r").readlines()
+inputlines = [line.strip() for line in inputlines]
+
+positions = []
+velocities = []
+for line in inputlines:
+    # e.g.:p=0,4 v=3,-3
+    numbers = [int(x) for x in re.findall(r"[-\d]+", line)]
+    print(numbers)
+
+    positions.append(np.array(numbers[:2]))
+    velocities.append(np.array(numbers[2:]))
+
+sim_len = 100
+
+# add the velocity sim_len times to the position
+positions = np.array(positions)
+velocities = np.array(velocities)
+
+velocities = velocities * sim_len
+positions = positions + velocities
+
+# modulo with the map size
+positions = positions % MAP_SIZE
+
+for y in range(MAP_SIZE[1]):
+    for x in range(MAP_SIZE[0]):
+        if [x, y] in positions:
+            # count the occurence
+            cnt = positions.tolist().count([x, y])
+            if cnt > 0:
+                print(f"{cnt}", end="")
+            else:
+                print(".", end="")
+        else:
+            print(".", end="")
+    print()
+
+
+def get_quadrant(x, y):
+    if x < MAP_SIZE[0] / 2 - 1 and y < MAP_SIZE[1] / 2 - 1:
+        return 1
+    elif x > MAP_SIZE[0] / 2 and y < MAP_SIZE[1] / 2 - 1:
+        return 2
+    elif x < MAP_SIZE[0] / 2 - 1 and y > MAP_SIZE[1] / 2:
+        return 3
+    elif x > MAP_SIZE[0] / 2 and y > MAP_SIZE[1] / 2:
+        return 4
+    else:
+        return 0  # belongs to no quadrant
+
+
+quadrant_counts = {}
+for p in positions:
+    x, y = p
+    quadrant = get_quadrant(x, y)
+
+    if quadrant not in quadrant_counts:
+        quadrant_counts[quadrant] = 0
+    quadrant_counts[quadrant] += 1
+
+for y in range(MAP_SIZE[1]):
+    for x in range(MAP_SIZE[0]):
+        q = get_quadrant(x, y)
+        print(f"{q}", end="")
+    print()
+
+for k in sorted(quadrant_counts.keys()):
+    print(f"{k}: {quadrant_counts[k]}")
+
+
+pprint(quadrant_counts.values())
+
+
+print(
+    f"part1: {quadrant_counts[1] * quadrant_counts[2] * quadrant_counts[3] * quadrant_counts[4]}"
+)  # 218965032
+
+positions = []
+velocities = []
+for line in inputlines:
+    # e.g.:p=0,4 v=3,-3
+    numbers = [int(x) for x in re.findall(r"[-\d]+", line)]
+    print(numbers)
+
+    positions.append(np.array(numbers[:2]))
+    velocities.append(np.array(numbers[2:]))
+
+
+positions = np.array(positions)
+velocities = np.array(velocities)
+
+# part2
+x_variances = []
+y_variances = []
+for sim in range(10000):
+    positions = positions + velocities
+    positions = positions % MAP_SIZE
+
+    xvariance = np.var(positions[:, 0])
+    yvariance = np.var(positions[:, 1])
+    x_variances.append(xvariance)
+    y_variances.append(yvariance)
+
+    if xvariance < 600 or yvariance < 600:
+        bw_image = np.zeros((MAP_SIZE[1], MAP_SIZE[0]), dtype=np.uint8)
+        for p in positions:
+            x, y = p
+            bw_image[y, x] = 255
+        # save to bmp
+        cv2.imwrite(f"14\\bmps\\{str(sim+1).zfill(5)}.bmp", bw_image)
+
+fig, ax = plt.subplots(nrows=3, ncols=1, sharex=True)
+ax[0].plot(x_variances)
+ax[1].plot(y_variances)
+ax[2].plot([x * y for x, y in zip(x_variances, y_variances)])
+plt.show()
+
+# part2 7037
